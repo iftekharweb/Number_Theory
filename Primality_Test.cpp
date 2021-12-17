3a4586aa734fea2b1c46041da3b03dd995261797
Iftekhar Md. Shishir <84434133+Ciftekhar@users.noreply.github.com>
2021-12-17 21:50:53 +0600

    Update Primality_Test.cpp

--- Primality_Test.cpp
+++ Primality_Test.cpp
@@ -1,10 +1,20 @@
-# Time complexity: O(n)
-# Native Approch
-
+1. Time complexity: O(n) | Native Approch
 bool isPrime(ll n)
 {
         if(n == 1) return false;
-        for(int i=2; i<n; i++) {
+        for(ll i=2; i<n; i++) {
+                if(n%i == 0) return false;
+        }
+        return true;
+}
+
+
+
+2. Time Complexity: O(sqrt(n)) | Better Approch 
+bool isPrime(ll n)
+{
+        if(n == 1) return false;
+        for(ll i=2; i*i<=n; i++) {
                 if(n%i == 0) return false;
         }
         return true;
