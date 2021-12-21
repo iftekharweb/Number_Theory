e77a1b0145a8a27236060bc18d2ad56784421278
Iftekhar Md. Shishir <84434133+Ciftekhar@users.noreply.github.com>
2021-12-21 17:46:15 +0600

    Update Prime_Factorization.cpp

--- Prime_Factorization.cpp
+++ Prime_Factorization.cpp
@@ -1,3 +1,4 @@
+
 1. Time Complexity: O(log n)  |  Brute Force Approch 
 Note: If the nuber itself is a prime number (say 10^9+7), the time complexity becomes O(n)
 
@@ -15,7 +16,7 @@ void primeFact(ll n)
                 }
         }
 }
-
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2. Time Complexity : O(sqrt n) | Optimized Approch
 
 vector<ll> factors;
@@ -37,4 +38,45 @@ void primeFact(ll n)
         }
 }
 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+3. Time Complexity : O(log n) | Sieve Approch
+NOTE : Prime factorization for all numbers from 2 to N using sieve.
+
+ll maxN = 1000001;
+bool mark[1000001];
+vector<ll> factorsFull[1000001], factors[1000001];
+void primeFactSeive()
+{
+        for(ll i = 1; i<maxN; i++) mark[i] = false;
+        mark[1] = true;
+        for(ll i=2; i<maxN; i++) {
+                if(!mark[i]) {
+                        for(ll j=i; j<maxN; j+=i) {
+                                mark[j] = true;
+                                factorsFull[j].push_back(i);
+                        }
+                }
+        }
+        for(int i=2; i<maxN; i++) {
+                ll value = i;
+                for(auto x:factorsFull[i]) {
+                        while(value%x==0) {
+                                value/=x;
+                                factors[i].push_back(x);
+                        }
+                }
+        }
+        for(int i=2; i<20; i++) {
+                cout << i << " = ";
+                printVector(factors[i]);
+        }
+}
+input:
+5
+Output:
+2 = 2
+3 = 3
+4 = 2 2
+5 = 5
+120 = 2 2 2 3 5
 
