0d613528e15b0e50c153153f05da3cf43b8ef68d
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2022-04-30 01:19:37 +0600

    Update Prime_Factorization.cpp

--- Prime_Factorization.cpp
+++ Prime_Factorization.cpp
@@ -3,17 +3,18 @@
 Note: If the nuber itself is a prime number (say 10^9+7), the time complexity becomes O(n)
 
 vector<ll> factors;
+vector<ll> factors;
 void primeFact(ll n)
 {
         for(ll i=2; i<=n; i++) {
-                if(n%i == 0) {
-                        int cnt = 0;
-                        while(n%i == 0) {
-                                cnt++, n/=i;
-                                factors.push_back(i);
-                        }
-                        cout << i << "^" << cnt << endl;
+            if(n%i == 0) {
+                int cnt = 0;
+                while(n%i == 0) {
+                    cnt++, n/=i;
+                    factors.push_back(i);
                 }
+                cout << i << "^" << cnt << endl;
+            }
         }
 }
 input:
@@ -30,19 +31,20 @@ vector<ll> factors;
 void primeFact(ll n)
 {
         for(ll i=2; i*i<=n; i++) {
-                if(n%i == 0) {
-                        int cnt = 0;
-                        while(n%i == 0) {
-                                cnt++, n/=i;
-                                factors.push_back(i);
-                        }
-                        cout << i << "^" << cnt << endl;
+            if(n%i == 0) {
+                int cnt = 0;
+                while(n%i == 0) {
+                    cnt++, n/=i;
+                    factors.push_back(i);
                 }
+                cout << i << "^" << cnt << endl;
+            }
         }
         if(n>1) {
-                factors.push_back(n);
-                cout << n << "^" << 1 << endl;
+            factors.push_back(n);
+            cout << n << "^" << 1 << endl;
         }
+        return;
 }
 input:
 120
